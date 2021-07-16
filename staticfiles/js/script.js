1843a96e0b8914955129d467daafa48fdb1043cb
Simone <yolodoggy911@gmail.com>
2021-07-16 01:21:54 -0700

    s

---
+++ staticfiles/js/script.js
@@ -0,0 +1,9 @@
+
+var titleInputClasses = document.querySelector('#id_title').classList;
+titleInputClasses.add('form-control')
+titleInputClasses.add('form-control-lg')
+titleInputClasses.add('my-5')
+
+
+var contentInputClasses = document.querySelector('#id_content').classList
+contentInputClasses.add('form-control')
