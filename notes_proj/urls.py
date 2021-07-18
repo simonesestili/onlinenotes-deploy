e4779fdf4dfbcf62ab3b4d2791e728ab5aa1c56a
Simone <yolodoggy911@gmail.com>
2021-07-18 14:05:24 -0700

    added custom 404

--- notes_proj/urls.py
+++ notes_proj/urls.py
@@ -20,5 +20,6 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notes_app.urls')),
-
 ]
+
+handler404 = 'notes_app.views.error_404'
