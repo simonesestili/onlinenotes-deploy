56ef4d61a7af0d281a9f3e6b2628d892b1dd58d3
Simone <yolodoggy911@gmail.com>
2021-07-20 12:14:48 -0700

    changed title to: The Note Taking App For Minimalists | OnlineNotes.me

--- notes_proj/urls.py
+++ notes_proj/urls.py
@@ -1,4 +1,5 @@
-"""notes_proj URL Configuration
+"""
+notes_proj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
